1947c001ff1676447fcc56110e4a34512d661b83
Ashley Jaynes <ashley.jaynes@mac.com>
2026-01-05 14:53:53 -0800

    minor styling fixes

--- app/_components/Card.tsx
+++ app/_components/Card.tsx
@@ -22,7 +22,7 @@ export default function Card({ title, thumbnailUrl, keywords = [], mediaType, id
 
   return (
     <Link href={`/item/${id}`}>
-      <article className="flex h-full flex-col overflow-hidden rounded-md border border-slate-200 bg-white shadow-md border-white hover:shadow-lg hover:shadow-gray-950 hover:border-blue-500">
+      <article className="flex h-full flex-col overflow-hidden rounded-md border bg-white shadow-md border-white hover:shadow-lg hover:shadow-gray-950 hover:border-blue-500">
         <div className="relative w-full overflow-hidden bg-slate-100">
           <div className="relative aspect-video">
             {thumbnailUrl && (
